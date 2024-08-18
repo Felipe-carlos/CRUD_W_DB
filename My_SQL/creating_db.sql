d74b1b3747ba01a562efadfab92cc2f0e53abcef
Felipe-carlos <felipe.carlos.eletrica@gmail.com>
2024-08-18 10:49:28 -0300

    adding Postgre version

---
+++ My_SQL/creating_db.sql
@@ -0,0 +1,11 @@
+create database crud_python;
+use crud_python;
+
+-- CREATE TABLE produtos(
+-- 	id int primary key auto_increment,
+--     nome varchar(50) Not null,
+--     preco decimal(8,2) not null,
+--     estoque int not null
+-- );
+
+
